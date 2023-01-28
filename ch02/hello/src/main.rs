165b3bac672bb09f02000a00061dd8d776e78c3d
jkarasha <jkarasha@gmail.com>
2023-01-27 16:50:48 -0800

    Add hello/handling-cla projects.

---
+++ ch02/hello/src/main.rs
@@ -0,0 +1,26 @@
+//simple introduction to rust/functions/variables
+fn main() {
+    println!("Called gcd: {}", gcd(5,7));
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+//add unit testing.
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14,15), 1);
+    assert_eq!(gcd(2*3*5*11*17,
+        3*7*11*13*19),
+        3 * 11);
+}
